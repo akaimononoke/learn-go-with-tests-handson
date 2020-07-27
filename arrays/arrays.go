38081095bde958a0d4f950807c48538794cab916
yakamon <p.m.byzance0817@gmail.com>
2020-07-27 22:13:16 +0900

    2020-07-27 22:13:16

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -1,6 +1,6 @@
 package arrays
 
-func Sum(nums [5]int) int {
+func Sum(nums []int) int {
 	sum := 0
 	for _, n := range nums {
 		sum += n
--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,16 +1,31 @@
 package arrays
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	nums := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		nums := []int{1, 2, 3, 4, 5}
+		got := Sum(nums)
+		want := 15
+		if got != want {
+			t.Errorf("with %v, got %d, want %d", nums, got, want)
+		}
+	})
+}
 
-	got := Sum(nums)
-	want := 15
-
-	if got != want {
-		t.Errorf("with %v, got %d, want %d", nums, got, want)
+func BenchmarkSum(b *testing.B) {
+	nums := []int{0, 2, 4, 6, 8, 10}
+	for i := 0; i < b.N; i++ {
+		Sum(nums)
 	}
+}
+
+func ExampleSum() {
+	nums := []int{0, 3, 6}
+	sum := Sum(nums)
+	fmt.Println(sum)
+	// Output: 9
 }
