3a30c0a3fa41e35bf06952f2c78e17bea0685f2e
yakamon <p.m.byzance0817@gmail.com>
2020-07-27 23:46:53 +0900

    2020-07-27 23:46:53

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,21 +14,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %v, want %v", got, want)
-		}
+	t.Parallel()
+	for desc, tt := range map[string]struct {
+		shape Shape
+		want  float64
+	}{
+		"rectangle": {shape: Rectangle{12, 6}, want: 72.0},
+		"circle":    {shape: Circle{10}, want: 314.1592653589793},
+		"triangle":  {shape: Triangle{12, 6}, want: 36.0},
+	} {
+		t.Run(desc, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %v, want %v", tt.shape, got, tt.want)
+			}
+		})
 	}
-
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
