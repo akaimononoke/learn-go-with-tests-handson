064b4efc73e933a9083caa12b90cd783cf7a15d8
yakamon <p.m.byzance0817@gmail.com>
2020-07-28 18:58:19 +0900

    2020-07-28 18:58:19 Select 1

---
+++ concurrency/select.go
@@ -0,0 +1,21 @@
+package concurrency
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	durationA := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	durationB := time.Since(startB)
+
+	if durationA < durationB {
+		return a
+	}
+	return b
+}
