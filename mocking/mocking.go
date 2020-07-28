799954c22654d6d9cd9d9eab8e11770ed1ef9284
yakamon <p.m.byzance0817@gmail.com>
2020-07-28 17:30:21 +0900

    2020-07-28 17:30:21 Mocking 2

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -22,6 +22,15 @@ func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func CountDown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		sleeper.Sleep()
@@ -32,6 +41,6 @@ func CountDown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	CountDown(os.Stdout, sleeper)
 }
