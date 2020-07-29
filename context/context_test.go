b0524ff54063559b1431412a6e41e149b9506ea5
yakamon <p.m.byzance0817@gmail.com>
2020-07-29 20:09:51 +0900

    2020-07-29 20:09:51 Context.1

---
+++ context/context_test.go
@@ -0,0 +1,77 @@
+package context
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+type SpyStore struct {
+	response  string
+	cancelled bool
+
+	t *testing.T
+}
+
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
+	return s.response
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
+func TestHandler(t *testing.T) {
+	t.Parallel()
+
+	data := "Hello, world"
+
+	t.Run("returns data from store", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		server := Server(store)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
+
+		server.ServeHTTP(res, req)
+
+		if res.Body.String() != data {
+			t.Errorf(`got "%s", want "%s"`, res.Body.String(), data)
+		}
+		store.assertWasNotCancelled()
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		store := &SpyStore{response: data, t: t}
+		server := Server(store)
+
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(req.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		req = req.WithContext(cancellingCtx)
+
+		res := httptest.NewRecorder()
+
+		server.ServeHTTP(res, req)
+
+		store.assertWasCancelled()
+	})
+}
+
+func (s *SpyStore) assertWasCancelled() {
+	s.t.Helper()
+	if !s.cancelled {
+		s.t.Errorf("store was not told to cancel")
+	}
+}
+
+func (s *SpyStore) assertWasNotCancelled() {
+	s.t.Helper()
+	if s.cancelled {
+		s.t.Errorf("it should not have cancelled the store")
+	}
+}
