aae6aed25766d86448a5f0c9e400b0ce5a0047b9
yakamon <p.m.byzance0817@gmail.com>
2020-07-29 20:51:54 +0900

    2020-07-29 20:51:54

--- context/context.go
+++ context/context.go
@@ -1,6 +1,7 @@
 package context
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
@@ -10,25 +11,17 @@ management of long-running processes
 */
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
+		data, err := store.Fetch(r.Context())
 
-		data := make(chan string, 1)
-
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-ctx.Done():
-			store.Cancel()
+		if err != nil {
+			return
 		}
+
+		fmt.Fprint(w, data)
 	}
 }
