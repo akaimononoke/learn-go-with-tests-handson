6434a8e125a1bcf9b5697a458947ce576d59163e
yakamon <p.m.byzance0817@gmail.com>
2020-07-29 17:58:09 +0900

    2020-07-29 17:58:09 Reflection.2 Complete

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,7 +3,39 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
+	walkValue := func(v reflect.Value) {
+		walk(v.Interface(), fn)
+	}
+	switch val := getValue(x); val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Array, reflect.Slice:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
+	case reflect.Func:
+		for _, result := range val.Call(nil) {
+			walk(result.Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	return val
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,25 +1,148 @@
 package reflection
 
 import (
+	"reflect"
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
+	t.Parallel()
 
-	x := struct {
-		Name string
-	}{expected}
+	for desc, test := range map[string]struct {
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		"Struct with one string field": {
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+		"Struct with two string fields": {
+			struct {
+				Name string
+				City string
+			}{"Chris", "London"},
+			[]string{"Chris", "London"},
+		},
+		"Struct with non string field": {
+			struct {
+				Name string
+				Age  int
+			}{"Chris", 33},
+			[]string{"Chris"},
+		},
+		"Nested fields": {
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		"Pointers to thing": {
+			&Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
+		},
+		"Slices": {
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		"Arrays": {
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+	} {
+		t.Run(desc, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	walk(x, func(input string) {
-		got = append(got, input)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+
+	t.Run("Maps", func(t *testing.T) {
+		m := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(m, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
 	})
 
-	if len(got) != 1 {
-		t.Errorf("wrong number of function calls, got %d want %d", len(got), 1)
+	t.Run("Channels", func(t *testing.T) {
+		ch := make(chan Profile)
+
+		go func() {
+			ch <- Profile{33, "Berlin"}
+			ch <- Profile{34, "Katowice"}
+			close(ch)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(ch, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Functions", func(t *testing.T) {
+		f := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(f, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	t.Helper()
+	for _, x := range haystack {
+		if x == needle {
+			return
+		}
 	}
-	if got[0] != expected {
-		t.Errorf("got %q, want %q", got[0], expected)
-	}
+	t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
 }
