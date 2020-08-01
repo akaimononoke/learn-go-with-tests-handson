beabdcd45e2e3687f0669580ed550f6cad0c4d44
yakamon <p.m.byzance0817@gmail.com>
2020-08-01 18:12:24 +0900

    2020-08-01 18:12:24 Refactor: DRY up showScore/processWin

--- application/server/server.go
+++ application/server/server.go
@@ -24,8 +24,7 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
@@ -33,18 +32,19 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
 	p.store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	case http.MethodPost:
-		p.processWin(w, r)
+		p.processWin(w, player)
 	}
 }
 
