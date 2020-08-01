6b48d32edb8817e674c1d6fcf1048537d323edd1
yakamon <p.m.byzance0817@gmail.com>
2020-08-01 17:34:10 +0900

    2020-08-01 17:34:10 Checking status code

--- application/server/server_test.go
+++ application/server/server_test.go
@@ -33,6 +33,7 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(res, req)
 
+		assertStatus(t, http.StatusOK, res.Code)
 		assertResponseBody(t, "20", res.Body.String())
 	})
 
@@ -42,13 +43,30 @@ func TestGetPlayers(t *testing.T) {
 
 		server.ServeHTTP(res, req)
 
+		assertStatus(t, http.StatusOK, res.Code)
 		assertResponseBody(t, "10", res.Body.String())
+	})
+
+	t.Run("returns 404 on missing players", func(t *testing.T) {
+		req := newGetScoreRequest("Apollo")
+		res := httptest.NewRecorder()
+
+		server.ServeHTTP(res, req)
+
+		assertStatus(t, http.StatusNotFound, res.Code)
 	})
 }
 
 func newGetScoreRequest(name string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
 	return req
+}
+
+func assertStatus(t *testing.T, want, got int) {
+	t.Helper()
+	if want != got {
+		t.Errorf("want status %q, got %q", want, got)
+	}
 }
 
 func assertResponseBody(t *testing.T, want, got string) {
