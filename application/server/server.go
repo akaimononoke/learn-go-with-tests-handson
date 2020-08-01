6deb71ee4c1179d3388e553fd1c6e67e079f60cb
yakamon <p.m.byzance0817@gmail.com>
2020-08-01 17:47:30 +0900

    2020-08-01 17:47:30 Refactor: isolate showScore/processWin from ServeHTTP

--- application/server/server.go
+++ application/server/server.go
@@ -31,20 +31,26 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
-	}
-
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	score := p.store.GetPlayerScore(player)
-
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
-
 	fmt.Fprint(w, score)
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		p.showScore(w, r)
+	case http.MethodPost:
+		p.processWin(w)
+	}
 }
 
 func main() {
