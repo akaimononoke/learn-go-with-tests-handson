883f5965f3e2fce981fdff6d19ecad56428e3be8
yakamon <p.m.byzance0817@gmail.com>
2020-08-01 16:50:35 +0900

    2020-08-01 16:50:35 Write server test

---
+++ application/server/server_test.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetPlayers(t *testing.T) {
+	t.Parallel()
+
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		req, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		res := httptest.NewRecorder()
+
+		PlayerServer(res, req)
+
+		want := "20"
+		got := res.Body.String()
+
+		if got != want {
+			t.Errorf("want %q, want %q", want, got)
+		}
+	})
+}
