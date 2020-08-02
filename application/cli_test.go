9aa5be59fed9c255fcb75cef770dd00840be7c74
akaimononoke <p.m.byzance0817@gmail.com>
2020-08-02 14:47:42 +0900

    2020-08-02 14:47:42 Add io.Reader to CLI for receiving user input

--- application/cli_test.go
+++ application/cli_test.go
@@ -1,15 +1,25 @@
 package application
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestCLI(t *testing.T) {
+	in := strings.NewReader("Chris wins\n")
+
 	playerStore := &StubPlayerStore{}
-	cli := &CLI{playerStore}
+	cli := &CLI{playerStore, in}
 	cli.PlayPoker()
 
 	if len(playerStore.winCalls) != 1 {
 		t.Fatal("a win call expected")
+	}
+
+	want := "Chris"
+	got := playerStore.winCalls[0]
+
+	if want != got {
+		t.Errorf("recorded winner is invalid: want %q, got %q", want, got)
 	}
 }
